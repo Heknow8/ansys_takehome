26754d1eccce2cbcd82754faeb1ca7a33e2fd108
Heknow8 <diamondubah783@gmail.com>
2025-03-07 19:08:55 +0000

    set nat gateway for private and public subnet

--- main.tf
+++ main.tf
@@ -38,3 +38,22 @@ resource "aws_internet_gateway" "igw" {
     Name = "main-igw"
   }
 }
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "main-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}

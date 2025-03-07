bffeba40e4b9a2985285805b6575a42d1f9ebf9c
Heknow8 <diamondubah783@gmail.com>
2025-03-07 19:12:32 +0000

    setup security groups

--- main.tf
+++ main.tf
@@ -97,3 +97,34 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_security_group" "web_sg" {
+  vpc_id     = aws_vpc.main.id
+  name       = "web-server-sg"
+  description = "Allow HTTP and SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["92.14.189.226/32"]  
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  tags = {
+    Name = "web-server-sg"
+  }
+}
